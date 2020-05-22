3d743413f859d3bec9ff02df96821173348b3e67
vouis <16nding@gmail.com>
2020-05-22 16:52:46 +0800

    build 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>404</div>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
