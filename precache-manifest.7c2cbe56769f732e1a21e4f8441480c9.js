8f05b91c72c18e6e9c1bbf66c458a2a8c7335579
vouis <16nding@gmail.com>
2020-05-24 01:21:36 +0800

    deploy

---
+++ precache-manifest.7c2cbe56769f732e1a21e4f8441480c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965668c18f08ff3ffd3a",
+    "url": "/record-for-money/css/app.c9c02208.css"
+  },
+  {
+    "revision": "65fe742f54108b9bc66b6526e4ad2125",
+    "url": "/record-for-money/index.html"
+  },
+  {
+    "revision": "965668c18f08ff3ffd3a",
+    "url": "/record-for-money/js/app.eac71dc8.js"
+  },
+  {
+    "revision": "4ca6b7fa7024f8d93211",
+    "url": "/record-for-money/js/chunk-vendors.cf392ba4.js"
+  },
+  {
+    "revision": "74f622a35951b60108a86d05e903b642",
+    "url": "/record-for-money/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/record-for-money/robots.txt"
+  }
+]);
