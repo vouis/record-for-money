cfd042466c7e41a066eda8b1cd4034a8713d2c89
vouis <16nding@gmail.com>
2020-05-24 00:19:19 +0800

    set CSS in nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template >
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item" active-class>
       <Icon name="money" />
       <span>记账</span>
-    </router-link>|
-    <router-link to="/labels">
+    </router-link>
+    <router-link to="/labels" class="item">
       <Icon name="label" />标签
-    </router-link>|
-    <router-link to="/statistics">
+    </router-link>
+    <router-link to="/statistics" class="item">
       <Icon name="statistic" />统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -19,4 +19,21 @@ export default Vue.extend({});
 </script >
 
 <style lang="scss" scoped>
+nav {
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .icon {
+    width: 30px;
+    height: 30px;
+  }
+}
 </style>
